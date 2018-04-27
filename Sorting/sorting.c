29e93baceea84a5e42d972ff50ebdc146a231cb4
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2018-04-27 15:31:38 +0100

    Moved sort function execution to function pointers.

--- Sorting/sorting.c
+++ Sorting/sorting.c
@@ -5,61 +5,38 @@
 #include "BubbleSort.h"
 #include "HeapSort.h"
 
+typedef void (* SortFunction)(Vector*, unsigned int);
+
+typedef void (* SortRangeFunction)(Vector*, unsigned int, unsigned int,
+                                   unsigned int);
+
+typedef struct {
+    SortFunction sortFunction;
+    SortRangeFunction sortRangeFunction;
+    char* name;
+} SortFunctionPair;
+
 int main() {
     printf("Hello! Let's sort this out!\n");
     unsigned int debug = 0;
     int v[] = {72, 29, 38, 22, 60, 2};
 
-    {
-        printf("\nSelection Sorting\n");
+    SortFunctionPair functions[] = {
+            {Sort_Selection_Vector, Sort_Selection_Vector_Range, "Selection Sort"},
+            {Sort_Insertion_Vector, Sort_Insertion_Vector_Range, "Insertion Sort"},
+            {Sort_Bubble_Vector,    Sort_Bubble_Vector_Range,    "Bubble Sort"},
+            {Sort_Heap_Vector,      Sort_Heap_Vector_Range,      "Heap Sort"}
+    };
+    int functionsSize = 4;
+
+    for (int i = 0; i < functionsSize; ++i) {
+        printf("\nRunning %s\n", functions[i].name);
         Vector* vector = Vector_createFromArrayCopy(v, 6);
 
         printf("Initial Array:\n");
         Vector_print(vector);
 
-        Sort_Selection_Vector(vector, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nInsertion Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Insertion_Vector(vector, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nBubble Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Bubble_Vector(vector, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nHeap Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Heap_Vector(vector, debug);
+        (*functions[i].sortFunction)(vector, debug);
 
         printf("Sorted Array:\n");
         Vector_print(vector);
@@ -71,56 +48,14 @@ int main() {
     printf("\nLet's sort just part of the vector, from index %1d to %1d\n", min,
            max);
 
-    {
-        printf("\nSelection Sorting\n");
+    for (int i = 0; i < functionsSize; ++i) {
+        printf("\nRunning %s\n", functions[i].name);
         Vector* vector = Vector_createFromArrayCopy(v, 6);
 
         printf("Initial Array:\n");
         Vector_print(vector);
 
-        Sort_Selection_Vector_Range(vector, min, max, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nInsertion Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Insertion_Vector_Range(vector, min, max, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nBubble Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Bubble_Vector_Range(vector, min, max, debug);
-
-        printf("Sorted Array:\n");
-        Vector_print(vector);
-
-        Vector_destroy(vector);
-    }
-    {
-        printf("\nHeap Sorting\n");
-        Vector* vector = Vector_createFromArrayCopy(v, 6);
-
-        printf("Initial Array:\n");
-        Vector_print(vector);
-
-        Sort_Heap_Vector_Range(vector, min, max, debug);
+        (*functions[i].sortRangeFunction)(vector, min, max, debug);
 
         printf("Sorted Array:\n");
         Vector_print(vector);
